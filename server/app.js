94d9811d39d1f2949fd8976f6665e8b34351a3bb
Ryan Silva <RyandotSilva@gmail.com>
2019-05-27 17:25:24 -0400

    Add middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
+//Middleware
+app.use('graphql', graphqlHTTP({}));
+
 app.listen(4000, () => {
 	console.log('Now listening for requests on port 4000');
 });
