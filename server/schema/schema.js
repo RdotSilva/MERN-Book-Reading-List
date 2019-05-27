5676d7177adb5a18e226555a1ceb69ab6ca5b298
Ryan Silva <RyandotSilva@gmail.com>
2019-05-27 17:35:09 -0400

    Add BookType name field

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const { GraphQLObjectType, GraphQLString } = graphql;
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	fields: () => ({
-		id: { type: GraphQLString }
+		id: { type: GraphQLString },
+		name: { type: GraphQLString }
 	})
 });
