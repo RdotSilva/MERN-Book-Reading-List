d7f621c6020c200ae8c1fa02da6f26ec5f525bfa
Ryan Silva <RyandotSilva@gmail.com>
2019-05-28 19:33:33 -0400

    Add dummy book data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// Dummy data
+var books = [
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
+
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	fields: () => ({
