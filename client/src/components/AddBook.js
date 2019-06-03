92c5957c9fb143327500693d5432065aa0773cec
Ryan Silva <RyandotSilva@gmail.com>
2019-06-03 19:22:12 -0400

    Add form into AddBook component

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -14,7 +14,24 @@ const getAuthorsQuery = gql`
 class AddBook extends Component {
 	render() {
 		return (
-		
+			<form id="add-book">
+				<div className="field">
+					<label>Book name:</label>
+					<input type="text" />
+				</div>
+				<div className="field">
+					<label>Genre:</label>
+					<input type="text" />
+				</div>
+				<div className="field">
+					<label>Author:</label>
+					<select>
+						<option>Select author</option>
+						{this.displayAuthors()}
+					</select>
+				</div>
+				<button>+</button>
+			</form>
 		);
 	}
 }
