52a579d35ab1c2001915973cd1ed0601664f3cb5
Ryan Silva <RyandotSilva@gmail.com>
2019-06-03 19:30:18 -0400

    Add displayAuthors method

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -12,6 +12,20 @@ const getAuthorsQuery = gql`
 `;
 
 class AddBook extends Component {
+	displayAuthors() {
+		const { data } = this.props;
+		if (data.loading) {
+			return <option disabled>Loading Authors</option>;
+		}
+		return data.authors.map(author => {
+			return (
+				<option key={author.id} value={author.id}>
+					{author.name}
+				</option>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<form id="add-book">
