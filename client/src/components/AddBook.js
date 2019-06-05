34cc9bb7bb3954593124605b350a5b09a7bfd223
Ryan Silva <RyandotSilva@gmail.com>
2019-06-04 21:15:53 -0400

    Fix typo

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -33,7 +33,7 @@ class AddBook extends Component {
 
 	render() {
 		return (
-			<form id="add-book" onSubmit={this.SubmitForm.bind(this)}>
+			<form id="add-book" onSubmit={this.submitForm.bind(this)}>
 				<div className="field">
 					<label>Book name:</label>
 					<input
